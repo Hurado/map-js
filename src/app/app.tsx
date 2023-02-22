e86adcca89cf957c83672a4858586f4f299b0d19
Arkadiusz Piaszczynski <piaszczynskiarkadiusz@gmail.com>
2023-02-22 10:45:59 +0100

    feat: :zap: Initial commit with basic configurations

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import Button from '../../ui/src/lib/button/button';
 
-import NxWelcome from './nx-welcome';
+import NxWelcome from './DirectionsMap';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="map-js" />
-
+      
       <div />
     </>
   );
